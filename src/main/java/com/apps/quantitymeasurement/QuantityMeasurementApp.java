d5afe6ed0400eb31dd94b36a495fcf7f1d683993
Shivam Kiran Pokale <SH20587176@wipro.com>
2026-01-30 14:17:53 +0530

    add. UC2 feet quantity

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -12,30 +12,84 @@ public class QuantityMeasurementApp {
         }
         @Override
         public boolean equals(Object obj){
-           if (this == obj) {
-               return true;
-           }
-           if (obj == null ||getClass()!= obj.getClass() ){
-               return false;
-           }
-           Feet other = (Feet) obj;
-           return Double.compare(this.value,other.value)==0;
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null ||getClass()!= obj.getClass() ){
+                return false;
+            }
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value,other.value)==0;
         }
     }
+    public  static class Inches{
+        private final double value;
+
+        public Inches(double value){
+            this.value = value;
+        }
+        @Override
+        public boolean equals(Object obj){
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null ||getClass()!= obj.getClass() ){
+                return false;
+            }
+            Inches other = (Inches) obj;
+            return Double.compare(this.value,other.value)==0;
+        }
+    }
+    public static void demonstrateFeetEquality(double v1, double v2){
+        Feet f1= new Feet(v1);
+        Feet f2= new Feet(v2);
+        System.out.println(f1.equals(f2));
+    }
+    public static void demonstrateInchesEquality(double v1, double v2){
+        Inches i1= new Inches(v1);
+        Inches i2= new Inches(v2);
+        System.out.println(i1.equals(i2));
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter 1st value in feet: ");
-        double value1= sc.nextDouble();
-        System.out.println("Enter 2nd value in feet: ");
-        double value2= sc.nextDouble();
-
-        Feet feet1= new Feet(value1);
-        Feet feet2= new Feet(value2);
-
-        boolean result= feet1.equals(feet2);
-
-        System.out.println(result);
-
+        System.out.println("1. Feet check");
+        System.out.println("2. Convert feet into inches ");
+        System.out.println("3. Inches check ");
+        System.out.print("Choose on: ");
+        int ip= sc.nextInt();
+        switch (ip) {
+            case 1: {
+                System.out.println("Enter 1st value in feet: ");
+                double v1 = sc.nextDouble();
+                System.out.println("Enter 2nd value in feet: ");
+                double v2 = sc.nextDouble();
+                demonstrateFeetEquality(v1, v2);
+                break;
+            }
+            case 2: {
+                System.out.println("Enter value in feet: ");
+                double v1 = sc.nextDouble();
+                System.out.println("Enter value in inches: ");
+                double v2 = sc.nextDouble();
+                double convertInches = v1 * 12;
+                System.out.println(convertInches == v2);
+                break;
+            }
+            case 3: {
+                System.out.println("Enter 1st value in Inch: ");
+                double v1 = sc.nextDouble();
+                System.out.println("Enter 2nd value in Inch: ");
+                double v2 = sc.nextDouble();
+                demonstrateInchesEquality(v1, v2);
+                break;
+            }
+            default: {
+                System.out.println();
+                break;
+            }
+        }
     }
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -39,6 +40,37 @@ public class QuantityMeasurementAppTest {
         Feet f1=new Feet(1.0);
         assertTrue(f1.equals(f1));
     }
+    //for inches
+    @Test
+    public void testInchesEquality_SameValue(){
+        Inches i1=new Inches(1.0);
+        Inches i2=new Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
 
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        Inches i1=new Inches(1.0);
+        Inches i2=new Inches(2.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison(){
+        Inches i1=new Inches(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_NonNumericInput(){
+        Inches i1=new Inches(1.0);
+        assertFalse(i1.equals("one"));
+    }
+
+    @Test
+    public void testInchesEquality_SameReferance(){
+        Inches i1=new Inches(1.0);
+        assertTrue(i1.equals(i1));
+    }
 
 }
