619b75c9b8e9facace85069796457b813e2e15ad
shivampokale <shivampokale1625@gmail.com>
2026-02-03 16:32:48 +0530

    add UC3

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        Length l1=new Length(1.0,Length.LengthUnit.FEET);
 
+        Length l1=new Length(1.0,Length.LengthUnit.FEET);
         Length l2=new Length(12.0,Length.LengthUnit.INCHES);
 
         System.out.println("are lengths equal? "+ l1.equals(l2));
